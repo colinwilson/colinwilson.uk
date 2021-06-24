ebe00871d8495f9bdc3442c7978558ec6bc61817
Colin Wilson <colin.wilson@protonmail.ch>
2021-06-24 20:23:20 +0100

    ⚡ update mobile nav to hide on scroll menu 📦 update hugo to 0.84.1

--- themes/colinwilson/static/js/nav_mobile.js
+++ themes/colinwilson/static/js/nav_mobile.js
@@ -1,49 +1,26 @@
-jQuery(document).ready(function($){
-	// browser window scroll (in pixels) after which the "menu" link is shown
-	var offset = 300;
+// (function () {
+// 	var nav = new Headroom(document.querySelector("#mobilenav"), {
+// 		tolerance: 5,
+// 		offset: 205,
+// 		classes: {
+// 			initial: "animated",
+// 			pinned: "slideUp",
+// 			unpinned: "slideDown"
+// 		}
+// 	});
+// 	nav.init();
+// }());
 
-	var navigationContainer = $('#cd-nav'),
-		mainNavigation = navigationContainer.find('#cd-main-nav mobilenav');
 
-	//hide or show the "menu" link
-	checkMenu();
-	$(window).scroll(function(){
-		checkMenu();
+(function () {
+	var mobilenav = document.querySelector("#mobilenav");
+
+	var headroom = new Headroom(mobilenav, {
+		tolerance: {
+			down: 10,
+			up: 20
+		},
+		offset: 15
 	});
-
-	//open or close the menu clicking on the bottom "menu" link
-	$('.cd-nav-trigger').on('click', function(){
-		$(this).toggleClass('menu-is-open');
-		//we need to remove the transitionEnd event handler (we add it when scolling up with the menu open)
-		mainNavigation.off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend').toggleClass('is-visible');
-
-	});
-
-	function checkMenu() {
-		if( $(window).scrollTop() > offset && !navigationContainer.hasClass('is-fixed')) {
-			navigationContainer.addClass('is-fixed').find('.cd-nav-trigger').one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(){
-				mainNavigation.addClass('has-transitions');
-			});
-		} else if ($(window).scrollTop() <= offset) {
-			//check if the menu is open when scrolling up
-			if( mainNavigation.hasClass('is-visible')  && !$('html').hasClass('no-csstransitions') ) {
-				//close the menu with animation
-				mainNavigation.addClass('is-hidden').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
-					//wait for the menu to be closed and do the rest
-					mainNavigation.removeClass('is-visible is-hidden has-transitions');
-					navigationContainer.removeClass('is-fixed');
-					$('.cd-nav-trigger').removeClass('menu-is-open');
-				});
-			//check if the menu is open when scrolling up - fallback if transitions are not supported
-			} else if( mainNavigation.hasClass('is-visible')  && $('html').hasClass('no-csstransitions') ) {
-					mainNavigation.removeClass('is-visible has-transitions');
-					navigationContainer.removeClass('is-fixed');
-					$('.cd-nav-trigger').removeClass('menu-is-open');
-			//scrolling up with menu closed
-			} else {
-				navigationContainer.removeClass('is-fixed');
-				mainNavigation.removeClass('has-transitions');
-			}
-		}
-	}
-});
+	headroom.init();
+})();
