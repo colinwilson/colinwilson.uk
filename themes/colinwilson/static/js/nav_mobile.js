e4f4a4253196e8acfc7e5f29f6552df112240245
Colin Wilson <colin.wilson@protonmail.ch>
2021-06-25 01:29:45 +0100

    ⚡ mobi nav js remove cmments

--- themes/colinwilson/static/js/nav_mobile.js
+++ themes/colinwilson/static/js/nav_mobile.js
@@ -1,17 +1,3 @@
-// (function () {
-// 	var nav = new Headroom(document.querySelector("#mobilenav"), {
-// 		tolerance: 5,
-// 		offset: 205,
-// 		classes: {
-// 			initial: "animated",
-// 			pinned: "slideUp",
-// 			unpinned: "slideDown"
-// 		}
-// 	});
-// 	nav.init();
-// }());
-
-
 (function () {
 	var mobilenav = document.querySelector("#mobilenav");
 
